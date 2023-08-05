8a71efa2b5d3974b52217aca6b80f91400ea5bf7
GiacomoGiovannetti <giovannettii.giacomo@gmail.com>
2023-08-05 12:40:21 +0200

    aggiunto il font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"], 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        GentiumBookPlus : ['Gentium Book Plus', 'serif'],
+      }
+
+    },
   },
   plugins: [],
 }
